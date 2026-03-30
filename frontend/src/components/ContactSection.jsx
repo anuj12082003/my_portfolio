89eace3572c2225446eb4d5dd7ab052d688d8f3a
Anujguptahub <anuj9284gupta@gmail.com>
2026-03-30 16:14:01 +0530

    update API URL for production

--- frontend/src/components/ContactSection.jsx
+++ frontend/src/components/ContactSection.jsx
@@ -26,7 +26,7 @@ function ContactSection() {
 
     try {
 
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://my-portfolio-backend-qnv5.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
